15c65995692ba611ea45da9b56d8aebefca74dc6
Morten Sørensen <starswifter@gmail.com>
2010-01-18 19:39:09 +0100

    Added function to follow a line

--- src/motion/motion.h
+++ src/motion/motion.h
@@ -3,7 +3,12 @@
 
 #include "../mission/task.h"
 
+#define GO_STRAIGHT 0
+#define GO_LEFT -1
+#define GO_RIGHT 1
+
 void forward(int speed, double expected_distance, task_data_t * task_data);
 double speed_calc(double max_speed, double current_distance, double expected_distance);
+void follow_line(int direction, double max_speed, int line_color, struct calibration * calibration);
 
 #endif
